e237324a6b1aded6c2f4e2cc18ef9ac89b7a689c
WBAproject <afiasaad16@gmail.com>
2026-05-13 16:31:49 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,45 @@
+
+const categoryCards = document.querySelectorAll(".category-card");
+const searchInput = document.querySelector(".search-box input");
+const searchButton = document.querySelector(".search-btn");
+
+categoryCards.forEach(function(card){
+card.addEventListener("click",function(){
+const categoryName = card.querySelector("h3").textContent.trim();
+if (categoryName === "Books"){
+window.location.href = "Browse Items.html#books";}
+
+else if (categoryName === "Study Tools"){
+window.location.href = "Browse Items.html#tools";}
+
+else if (categoryName === "Electronics"){
+window.location.href = "Browse Items.html#electronics";}
+});
+});
+
+function searchProducts(){
+const searchText = searchInput.value.trim();
+
+if (searchText === "") {
+alert("Please enter item name");
+return;}
+
+window.location.href = "Browse Items.html?search=" + searchText;}
+
+searchButton.addEventListener("click",searchProducts);
+searchInput.addEventListener("keyup",function(event){
+if(event.key === "Enter"){
+searchProducts();}
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+const isLoggedIn = localStorage.getItem("isLoggedIn");
+const loginBtn = document.getElementById("login-btn");
+const userIcon = document.getElementById("user-profile");
+if (isLoggedIn === "true") {
+if (loginBtn) loginBtn.style.display = "none";
+if (userIcon) userIcon.style.display = "flex";}
+else {
+if (loginBtn) loginBtn.style.display = "block";
+if (userIcon) userIcon.style.display = "none";}
+});
